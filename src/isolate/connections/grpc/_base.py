19112ba7730d56cb95b9c052fa26166f163e4695
Batuhan Taskaya <isidentical@gmail.com>
2022-10-31 17:09:08 +0300

    Try increasing the agent delay

--- src/isolate/connections/grpc/_base.py
+++ src/isolate/connections/grpc/_base.py
@@ -35,7 +35,7 @@ class GRPCExecutionBase(EnvironmentConnection):
         self,
         function: definitions.SerializedObject,
         *,
-        max_wait_timeout: float = 5,
+        max_wait_timeout: float = 10,
     ) -> Iterator[definitions.PartialRunResult]:
         """Starts a new agent process, sends the given 'function' to the agent and
         awaits until the result is received. This function takes and returns raw gRPC
